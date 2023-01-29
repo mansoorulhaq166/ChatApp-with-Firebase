58e695fa4d53d4518d3fee1bcdf4a39069234338
Mansoor ul haq <Mansoorulhaq166@gmail.com>
2023-01-29 17:03:37 +0500

    first commit

---
+++ app/src/main/java/com/example/chatappfirebase/LoginActivity.java
@@ -0,0 +1,130 @@
+package com.example.chatappfirebase;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.method.PasswordTransformationMethod;
+import android.text.style.UnderlineSpan;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
+import com.basgeekball.awesomevalidation.utility.RegexTemplate;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+    TextView forgotPassword;
+    TextView signUp, sign_report;
+    EditText email, password;
+    Button login;
+    FirebaseAuth auth;
+    ProgressDialog progressDialog;
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        forgotPassword = (TextView) findViewById(R.id.forgot_password);
+        signUp = (TextView) findViewById(R.id.sign_up);
+        sign_report = (TextView) findViewById(R.id.signin_report);
+        email = (EditText) findViewById(R.id.enter_email);
+        login = findViewById(R.id.button_login);
+        password = (EditText) findViewById(R.id.enter_password);
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Please Wait..");
+        progressDialog.setCancelable(false);
+
+        auth = FirebaseAuth.getInstance();
+
+        Drawable drawable = password.getCompoundDrawables()[2];
+
+        AwesomeValidation mAwesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
+        mAwesomeValidation.addValidation(this, R.id.enter_password, RegexTemplate.NOT_EMPTY, R.string.error);
+        mAwesomeValidation.addValidation(this, R.id.enter_email, RegexTemplate.NOT_EMPTY, R.string.error);
+
+        password.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                final int DRAWABLE_RIGHT = 2;
+                if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                    if (motionEvent.getRawX() >= (password.getRight() - password.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+                        if (password.getTransformationMethod() instanceof PasswordTransformationMethod) {
+                            password.setTransformationMethod(null);
+                            drawable.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_IN);
+                            drawable.setAlpha(255);
+                        } else {
+                            password.setTransformationMethod(new PasswordTransformationMethod());
+                            drawable.setColorFilter(Color.BLACK, PorterDuff.Mode.SRC_IN);
+                            drawable.setAlpha(255);
+                        }
+                        // password.performClick();
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+        SpannableString forget = new SpannableString("Forgot Password ?");
+        forget.setSpan(new UnderlineSpan(), 0, forget.length(), 0);
+        forgotPassword.setText(forget);
+
+        SpannableString sign_up = new SpannableString("Don't have an account, Sign Up!");
+        sign_up.setSpan(new UnderlineSpan(), 23, sign_up.length(), 0);
+        signUp.setText(sign_up);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+
+        if (auth.getCurrentUser() != null) {
+            startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+        }
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mAwesomeValidation.validate()) {
+                    progressDialog.show();
+                    String StEmail = email.getText().toString();
+                    String StPassword = password.getText().toString();
+
+                    auth.signInWithEmailAndPassword(StEmail, StPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                progressDialog.dismiss();
+                                startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+                                finish();
+                            } else {
+                                sign_report.setText("Error in Login");
+                                sign_report.setTextColor(getResources().getColor(R.color.red));
+                                progressDialog.dismiss();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/chatappfirebase/RegisterActivity.java
@@ -0,0 +1,190 @@
+package com.example.chatappfirebase;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
+import com.basgeekball.awesomevalidation.utility.RegexTemplate;
+import com.example.chatappfirebase.Models.Users;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.Objects;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    EditText regName, regEmail, regPassword, regConPassword;
+    Button regSubmit;
+    TextView alreadyLogin;
+    CircleImageView profileImage;
+    FirebaseAuth auth;
+    FirebaseDatabase database;
+    FirebaseStorage storage;
+    Uri imageUri;
+    String imageUrl;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        alreadyLogin = findViewById(R.id.already_login);
+        regEmail = findViewById(R.id.reg_email);
+        regName = findViewById(R.id.reg_name);
+        regPassword = findViewById(R.id.reg_password);
+        regConPassword = findViewById(R.id.reg_confirm_password);
+        regSubmit = findViewById(R.id.reg_submit);
+        profileImage = (CircleImageView) findViewById(R.id.profile_image);
+
+        // Initializing Firebase instance
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        storage = FirebaseStorage.getInstance();
+
+        // using underline texts
+        SpannableString already = new SpannableString("Already have an account? Login!");
+        already.setSpan(new UnderlineSpan(), 25, already.length(), 0);
+        alreadyLogin.setText(already);
+
+        // using validation
+        AwesomeValidation mAwesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
+        mAwesomeValidation.addValidation(this, R.id.reg_email, Patterns.EMAIL_ADDRESS, R.string.err_email);
+        mAwesomeValidation.addValidation(this, R.id.reg_name, RegexTemplate.NOT_EMPTY, R.string.err_name);
+
+        mAwesomeValidation.addValidation(this, R.id.reg_password,
+                "(?=.*[\\d])(?=.*[~`!@#\\$%\\^&\\*\\(\\)\\-_\\+=\\{\\}\\[\\]\\|\\;:\"<>,./\\?]).{8,}",
+                R.string.err_password);
+
+        mAwesomeValidation.addValidation(this, R.id.reg_password, RegexTemplate.NOT_EMPTY, R.string.error);
+        mAwesomeValidation.addValidation(this, R.id.reg_confirm_password, RegexTemplate.NOT_EMPTY, R.string.error);
+        mAwesomeValidation.addValidation(this, R.id.reg_email, RegexTemplate.NOT_EMPTY, R.string.error);
+
+        alreadyLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent login_Activity = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(login_Activity);
+            }
+        });
+        regSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mAwesomeValidation.validate()) {
+                    String name = regName.getText().toString();
+                    String email = regEmail.getText().toString();
+                    String password = regPassword.getText().toString();
+                    String confirmPassword = regConPassword.getText().toString();
+
+                    auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                DatabaseReference reference = database.getReference().child("user")
+                                        .child(auth.getUid());
+                                StorageReference storageReference = storage.getReference().child("upload")
+                                        .child(auth.getUid());
+                                if (imageUri != null) {
+                                    storageReference.putFile(imageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                                            if (task.isSuccessful()) {
+                                                storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                                    @Override
+                                                    public void onSuccess(Uri uri) {
+                                                        imageUrl = uri.toString();
+                                                        Users user = new Users(auth.getUid(), name, email,
+                                                                imageUrl);
+                                                        reference.setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                            @Override
+                                                            public void onComplete(@NonNull Task<Void> task) {
+                                                                if (task.isSuccessful()) {
+                                                                    startActivity(new Intent(RegisterActivity.this, HomeActivity.class));
+                                                                    finish();
+                                                                } else {
+                                                                    Toast.makeText(RegisterActivity.this, "Error in Creating User", Toast.LENGTH_SHORT).show();
+                                                                }
+                                                            }
+                                                        });
+                                                    }
+                                                });
+                                            }
+                                        }
+                                    });
+                                } else {
+                                    imageUrl = "https://firebasestorage.googleapis.com/v0/b/chat-app-firebase-e531b.appspot.com/o/default.jpg?alt=media&token=b165b443-0524-4b07-808e-727c5170daf5";
+                                    Users user = new Users(auth.getUid(), name, email,
+                                            imageUrl);
+                                    reference.setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
+                                                startActivity(new Intent(RegisterActivity.this, HomeActivity.class));
+                                                finish();
+                                            } else {
+                                                Toast.makeText(RegisterActivity.this, "Error in Creating User", Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
+                                    });
+                                }
+
+
+                            } else {
+                                Toast.makeText(RegisterActivity.this, "Something went Wrong", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+        profileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                selectImageLauncher.launch("image/*");
+
+            }
+        });
+
+    }
+
+    private final ActivityResultLauncher<String> selectImageLauncher = registerForActivityResult(
+            new ActivityResultContracts.GetContent(),
+            new ActivityResultCallback<Uri>() {
+                @Override
+                public void onActivityResult(Uri uri) {
+                    if (uri != null) {
+                        imageUri = uri;
+                        profileImage.setImageURI(uri);
+                    }
+                }
+            });
+}
